6803469e8e430d340083c809eacfa0c1c3f8d640
PAEMOFF <110742258+PAJUSTD@users.noreply.github.com>
2026-03-15 01:54:11 +0500

    lesgo

---
+++ script.js
@@ -0,0 +1,18 @@
+const knopka = document.getElementById('my-btn');
+
+knopka.onclick = function() {
+    // 1. Каждый клик создаем НОВЫЙ объект звука
+    // Это позволяет им играть одновременно
+    const newAudio = new Audio('sound.mp3'); 
+    
+    // 2. Запускаем его
+    newAudio.play();
+
+    // 3. (Опционально) Чтобы не засорять память, 
+    // удаляем объект после того, как звук закончится
+    newAudio.onended = function() {
+        this.remove(); 
+    };
+
+    console.log("Бах! Еще один звук наложился");
+};
